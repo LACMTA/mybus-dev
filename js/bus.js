536427dd80788997fc75c4f225a7a65babc92e43
Nina Kin <matikin9@gmail.com>
2021-05-24 09:56:45 -0700

    tweak results page styles and behavior

--- js/bus.js
+++ js/bus.js
@@ -5,7 +5,35 @@ const LINE = URLPARAMS.get('line');
 const STOP1 = URLPARAMS.get('stop1');
 const STOP2 = URLPARAMS.get('stop2');
 
-const DATA_LINES = 'data/';
-const DATA_STOPS = 'data/';
+const DATA_LINES = 'data/line_changes.json';
+const DATA_STOPS = 'data/stop_changes.json';
+
+$.getJSON(DATA_LINES, showLineChanges);
+$.getJSON(DATA_STOPS, showStopChanges);
 
 
+function showStopChanges(data) {
+    $.each(data, 
+        function(key, val) {
+            
+        }
+    );
+}
+
+function showLineChanges(data) {
+    $.each(data, 
+        function(key, val) {
+            
+        }
+    );
+}
+
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
+
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+    trigger: 'focus'
+  });
